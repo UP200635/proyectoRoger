dcec1c305d793a135e37ff403904b0e2f7ec40aa
UP200635 <77815360+UP200635@users.noreply.github.com>
2023-08-09 13:10:31 -0600

    Add files via upload

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createConnection } from 'mysql2/promise';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const pool = createConnection({
+    host: 'mysqldb',
+    port: 3306,
+    user: 'upxxx-ifra',
+    password: '1234',
+    database: 'test'
+});
+
+app.get("/", (req, res) => {
+    res.json("Hola desde el back");
+});
+
+app.get("/peliculas", async (request, response) => {
+    try{
+        const [rows] = await pool.query("SELECT * FROM pelicula");
+        response.json(rows);
+    } catch (error){
+        response.status(500).json({ error: "Internal server error" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server on port 3000");
+});
